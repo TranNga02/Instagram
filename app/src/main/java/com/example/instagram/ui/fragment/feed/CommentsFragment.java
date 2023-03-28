938110c41a92ae6c6a08b5b88aaddd0c217e9a81
TranNga02 <tranngadn02@gmail.com>
2023-03-28 08:36:14 +0700

    merge

--- app/src/main/java/com/example/instagram/ui/fragment/feed/CommentsFragment.java
+++ app/src/main/java/com/example/instagram/ui/fragment/feed/CommentsFragment.java
@@ -1,4 +1,4 @@
-package com.example.instagram;
+package com.example.instagram.ui.fragment.feed;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.instagram.R;
 
 /**
  * A simple {@link Fragment} subclass.
